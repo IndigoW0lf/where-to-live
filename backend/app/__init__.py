e069ba8a4dcea48c41822486dbe0b50da90db00d
Kai Indigo Wolf <kai.indigo.wolf@gmail.com>
2024-02-26 10:22:49 -0600

    Add CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import main
+
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('config.py')
+    app.config.from_pyfile("config.py")
+    CORS(app)
     app.register_blueprint(main)
     return app
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -6,28 +6,8 @@ main = Blueprint("main", __name__)
 
 @main.route("/data")
 def get_data():
-    # Assuming 'data.csv' is in the 'backend/data' directory
     df = pd.read_csv("backend/data/data.csv")
-
-    # Transform DataFrame to desired format
-    transformed_data = df[
-        [
-            "Category",
-            "Kailua, Hawaii",
-            "Columbia, Missouri",
-            "Sacramento, California",
-            "Portland, Oregon",
-            "Charlotte, North Carolina",
-            "Seattle, Washington",
-        ]
-    ].melt(id_vars=["Category"], var_name="City", value_name="Population")
-
-    # Convert transformed DataFrame to JSON
+    transformed_data = df.melt(
+        id_vars=["Category"], var_name="City", value_name="Value"
+    )
     return jsonify(transformed_data.to_dict(orient="records"))
-
-
-@main.route("/population_data")
-def population_data():
-    df = pd.read_csv("path/to/your/data.csv")
-    population_data = df.loc["Population Estimates, July 1, 2022, (V2022)"].to_dict()
-    return jsonify(population_data)
